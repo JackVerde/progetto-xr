eda5c117d25ee5ce1076737d7d8e33316ed636d4
Marcello Bertoli <probobbo@gmail.com>
2019-11-19 11:09:52 +0100

    Added: spaceship, car and parent changer

---
+++ Assets/Scripts/GrabbableParentSetter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Throwable))]
+public class GrabbableParentSetter : MonoBehaviour
+{
+    private Throwable throwable;
+    private Transform table;
+
+    private void Start()
+    {
+        throwable = GetComponent<Throwable>();
+        table = GameObject.FindWithTag("Table")?.transform;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("ENTER " + other.gameObject.name);
+        if (!other.gameObject.CompareTag("Table")) return;
+        if (table != null && transform.parent==null)
+            transform.parent = table;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        Debug.Log("EXIT " + other.gameObject.name);
+        if (!other.gameObject.CompareTag("Table")) return;
+        if (transform.parent!=null && transform.parent.Equals(other.gameObject.transform))
+            transform.parent = null;
+    }
+}

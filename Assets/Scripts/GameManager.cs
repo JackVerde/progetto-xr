35ba4a04f1ec8da4e916f57b374c65ec7566a93c
Marcello Bertoli <probobbo@gmail.com>
2019-11-19 12:57:27 +0100

    Fix: modified rudder colliders and now application quits with escape

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+    }
+}

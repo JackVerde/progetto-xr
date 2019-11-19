0d3bb60cfd3684e3a8785e889e3efe77479c15d0
Marcello Bertoli <probobbo@gmail.com>
2019-11-19 12:05:36 +0100

    Added: Skybox, respawner

--- Assets/Scripts/GrabbableController.cs
+++ Assets/Scripts/GrabbableController.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 using Valve.VR.InteractionSystem;
 
 [RequireComponent(typeof(Throwable))]
-public class GrabbableParentSetter : MonoBehaviour
+public class GrabbableController : MonoBehaviour
 {
     private Throwable throwable;
     private Transform table;
+    private Vector3 initPosition;
+    private Quaternion initRotation;
 
     private void Start()
     {
         throwable = GetComponent<Throwable>();
         table = GameObject.FindWithTag("Table")?.transform;
+        var t = transform;
+        initPosition = t.position;
+        initRotation = t.rotation;
     }
 
     private void OnCollisionEnter(Collision other)
@@ -30,5 +35,14 @@ public class GrabbableParentSetter : MonoBehaviour
         if (!other.gameObject.CompareTag("Table")) return;
         if (transform.parent!=null && transform.parent.Equals(other.gameObject.transform))
             transform.parent = null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Respawn")) return;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        var t = transform;
+        t.position = initPosition;
+        t.rotation = initRotation;
     }
 }

5c79832dc49268c13d066f07acbda73103bf423c
Marcello Bertoli <probobbo@gmail.com>
2019-11-20 17:56:51 +0100

    Added: AR App completed

---
+++ Assets/Scripts/AR/AircraftController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+namespace AR
+{
+    public class AircraftController : MonoBehaviour
+    {
+        [SerializeField] private ARRaycastManager arRaycastManager;
+        private Transform aircraft;
+        private Vector2 screenPoint;
+        private bool placed;
+        private bool rotating;
+        public float rotationSpeed;
+
+        private void Start()
+        {
+            Application.targetFrameRate = 60;
+            placed = false;
+            aircraft = transform.GetChild(0);
+            screenPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        }
+
+        private void Update()
+        {
+            if (!placed)
+            {
+                var output = new List<ARRaycastHit>();
+                arRaycastManager.Raycast(screenPoint, output, TrackableType.Planes);
+                if (output.Count <= 0)
+                {
+                    aircraft.gameObject.SetActive(false);
+                    return;
+                }
+
+                aircraft.gameObject.SetActive(true);
+                transform.position = output[0].pose.position;
+                if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+                    placed = true;
+            }
+            else
+            {
+                if (Input.touchCount > 0)
+                {
+                    var touch = Input.GetTouch(0);
+                    if (touch.phase.Equals(TouchPhase.Moved))
+                    {
+                        var delta = touch.deltaPosition.x;
+                        aircraft.transform.Rotate(Vector3.up,-delta*Time.deltaTime*rotationSpeed);
+                    }
+                }
+            }
+        }
+
+        public void ResetPlaced()
+        {
+            placed = false;
+        }
+    }
+}
---
+++ Assets/Scripts/AR/LightEstimation.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(Light))]
+public class LightEstimation : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The ARCameraManager which will produce frame events containing light estimation information.")]
+    private ARCameraManager arCameraManager;
+
+    [SerializeField] private ReflectionProbe reflectionProbe;
+
+    public float lightIntensityMultiplier;
+    public float probeIntensityMultiplier;
+    public float globalIntensityMultiplier;
+
+    public float min, max;
+
+    public ARCameraManager CameraManager
+    {
+        get => arCameraManager;
+        set
+        {
+            if (arCameraManager == value)
+                return;
+
+            if (arCameraManager != null)
+                arCameraManager.frameReceived -= FrameChanged;
+
+            arCameraManager = value;
+
+            if (arCameraManager != null & enabled)
+                arCameraManager.frameReceived += FrameChanged;
+        }
+    }
+
+    private float? Brightness { get; set; }
+
+    private float? ColorTemperature { get; set; }
+
+    private Color? ColorCorrection { get; set; }
+
+    private void Awake()
+    {
+        _mLight = GetComponent<Light>();
+    }
+
+    private void OnEnable()
+    {
+        if (arCameraManager != null)
+            arCameraManager.frameReceived += FrameChanged;
+    }
+
+    private void OnDisable()
+    {
+        if (arCameraManager != null)
+            arCameraManager.frameReceived -= FrameChanged;
+    }
+
+    private void FrameChanged(ARCameraFrameEventArgs args)
+    {
+        if (args.lightEstimation.averageBrightness.HasValue)
+        {
+            Brightness = args.lightEstimation.averageBrightness.Value;
+            var intensityValue = Brightness.Value;
+            _mLight.intensity = Mathf.Clamp(intensityValue * lightIntensityMultiplier, min, max);
+            reflectionProbe.intensity = intensityValue * probeIntensityMultiplier; 
+            RenderSettings.ambientIntensity = intensityValue * globalIntensityMultiplier;
+        }
+
+        if (args.lightEstimation.averageColorTemperature.HasValue)
+        {
+            ColorTemperature = args.lightEstimation.averageColorTemperature.Value;
+            _mLight.colorTemperature = ColorTemperature.Value;
+        }
+
+        if (!args.lightEstimation.colorCorrection.HasValue) return;
+        ColorCorrection = args.lightEstimation.colorCorrection.Value;
+        _mLight.color = ColorCorrection.Value;
+    }
+    
+    private Light _mLight;
+}
